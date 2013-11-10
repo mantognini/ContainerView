04535a3eafce8dbe7f87e4354517c2f4a47b63b1
Marco Antognini <antognini.marco@gmail.com>
2013-11-10 10:20:42 +0100

    Initial Commit

---
+++ ContainerView/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ContainerView
+//
+//  Created by Marco Antognini on 10/11/13.
+//  Copyright (c) 2013 Marco Antognini. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
