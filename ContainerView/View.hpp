dd490091ea9fbb39a728054c8fb0b40661117ff2
Marco Antognini <antognini.marco@gmail.com>
2013-11-10 17:23:57 +0100

    add basic view/constview

---
+++ ContainerView/View.hpp
@@ -0,0 +1,177 @@
+//
+//  View.hpp
+//  ContainerView
+//
+//  Created by Marco Antognini on 10/11/13.
+//  Copyright (c) 2013 Marco Antognini. All rights reserved.
+//
+
+
+#ifndef CONTAINERVIEW_VIEW_HPP
+#define CONTAINERVIEW_VIEW_HPP
+
+
+
+#include <iterator>
+#include <utility>
+#include <type_traits>
+
+
+
+// Predeclarations
+
+template <class T, template <class...> class C>
+class View;
+
+template <class T, template <class...> class C>
+class ConstView;
+
+
+
+// Helpers
+
+template <class C>
+using IteratorCategory = typename C::iterator::iterator_category;
+
+template <class C>
+using IteratorType = typename C::iterator::value_type;
+
+template <class C>
+using IteratorDistance = typename C::iterator::difference_type;
+
+template <class C>
+using IteratorPointer = typename C::iterator::pointer;
+
+template <class C>
+using IteratorReference = typename C::iterator::reference;
+
+template <class C>
+using IteratorFromContainer = std::iterator<IteratorCategory<C>, IteratorType<C>, IteratorDistance<C>, IteratorPointer<C>, IteratorReference<C>>;
+
+
+
+// Iterators for View
+
+template <class T, template <class...> class C>
+class ViewIterator : public IteratorFromContainer<C<T>>
+{
+    using Container = C<T>;
+    using type = typename Container::iterator;
+    type self; // The underlying iterator
+
+    friend View<T, C>;
+
+    ViewIterator(type it) : self(it) { }
+
+public:
+    using typename IteratorFromContainer<Container>::reference;
+    using SELF = ViewIterator<T, C>;
+
+    ViewIterator& operator++() { ++self; return *this; }
+    reference operator*() { return *self; }
+
+    // Free function !=
+    friend
+    bool operator!=(SELF const& a, SELF const& b)
+    {
+        return a.self != b.self;
+    }
+};
+
+
+
+// Basically the same as ViewIterator but returns const values
+
+template <class T, template <class...> class C>
+class ConstViewIterator : public IteratorFromContainer<C<T>>
+{
+    using Container = C<T>;
+    using type = typename Container::const_iterator;
+    type self; // The underlying iterator
+
+    friend View<T, C>;
+    friend ConstView<T, C>;
+
+    ConstViewIterator(type it) : self(it) { }
+
+public:
+    using const_reference =  typename IteratorFromContainer<Container>::value_type const&;
+    using SELF = ConstViewIterator<T, C>;
+
+    ConstViewIterator& operator++() { ++self; return *this; }
+    const_reference operator*() const { return *self; }
+
+    // Free function !=
+    friend
+    bool operator!=(SELF const& a, SELF const& b)
+    {
+        return a.self != b.self;
+    }
+};
+
+
+
+// Views
+
+template <class T, template <class...> class C>
+class ConstView
+{
+protected:
+    using Container = C<T>;
+    Container const& underlying_container;
+
+public:
+    using const_iterator = ConstViewIterator<T, C>;
+
+    ConstView(Container const& container) : underlying_container(container) { }
+
+    const_iterator begin() const { return { underlying_container.begin() }; }
+    const_iterator end() const { return { underlying_container.end() }; }
+};
+
+
+template <class T, template <class...> class C>
+class View : public ConstView<T, C>
+{
+    using Container = C<T>;
+    Container& underlying_container;
+
+public:
+    using iterator = ViewIterator<T, C>;
+    using Base = ConstView<T, C>;
+
+    View(Container& container) : ConstView<T, C>(container), underlying_container(container) { }
+
+    using Base::begin;
+    iterator begin() { return { underlying_container.begin() }; }
+    using Base::end;
+    iterator end() { return { underlying_container.end() }; }
+};
+
+
+
+// Helpers for the user
+
+template <class T, template <class...> class C>
+View<T, C> viewOf(C<T>& container) {
+    return { container };
+}
+
+template <class T, template <class...> class C>
+ConstView<T, C> constViewOf(C<T> const& container) {
+    return { container };
+}
+
+template <class C>
+using ViewTypeFromCT_t = decltype(viewOf(std::declval<C&>()));
+
+template <class C>
+using ConstViewTypeFromCT_t = decltype(constViewOf(std::declval<C const&>()));
+
+#define ViewTypeFromCTVar(container) ViewTypeFromCT_t<decltype(container)>
+#define ConstViewTypeFromCTVar(container) ConstViewTypeFromCT_t<decltype(container)>
+
+
+
+#endif // CONTAINERVIEW_VIEW_HPP
+
--- ContainerView/main.cpp
+++ ContainerView/main.cpp
@@ -6,13 +6,124 @@
 //  Copyright (c) 2013 Marco Antognini. All rights reserved.
 //
 
+//#include <iostream>
+//#include <typeinfo>
+//#include <vector>
+//#include <scoped_allocator>
+//
+//template <template <class...> class C, class T>
+//struct Helper
+//{
+//    using allocator = typename C<T>::allocator_type;
+//};
+//
+//template <class T>
+//using Vector = std::vector<T, std::scoped_allocator_adaptor<std::allocator<T>>>;
+//
+//int main(int argc, const char * argv[])
+//{
+//    auto const& info = typeid(Helper<Vector, int>::allocator);
+//    auto const& info2 = typeid(Helper<std::vector, int>::allocator);
+//
+//    std::cout << "Name: " << info.name() << std::endl;
+//    std::cout << "Name: " << info2.name() << std::endl;
+//
+//    return 0;
+//}
+
+
+
+#include "View.hpp"
 #include <iostream>
+#include <typeinfo>
+#include <vector>
+#include <memory>
 
-int main(int argc, const char * argv[])
+class Element {
+    int x;
+public:
+    Element(int y) : x(y) { }
+
+    void print(int tab) const
+    {
+        for (int i = 0; i < tab; ++i)
+            std::cout << "\t";
+        std::cout << "Element::" << x << "\n";
+    }
+
+    void foo() { ++x; }
+};
+
+using Document = std::vector<Element>;
+using DocumentView = ViewTypeFromCT_t<Document>;
+using DocumentConstView = ConstViewTypeFromCT_t<Document>;
+
+void loadDocument(Document& doc)
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    for (auto i : { 2, 3, 5 })
+        doc.push_back(i);
 }
 
+void updateDocument(DocumentView view)
+{
+    for (auto& e : view)
+        e.foo();
+}
+
+void printDocument(DocumentConstView view)
+{
+    std::cout << "<Document>\n";
+    for (auto const& e : view)
+        e.print(1);
+    std::cout << "</Document>" << std::endl;
+}
+
+
+using AdvancedDocument = std::vector<std::shared_ptr<Element>>;
+using AdvancedDocumentView = ViewTypeFromCT_t<AdvancedDocument>;
+using AdvancedDocumentConstView = ConstViewTypeFromCT_t<AdvancedDocument>;
+
+void loadDocument(AdvancedDocument& doc)
+{
+    for (auto i : { 7, 11, 13 })
+        doc.emplace_back(std::make_shared<Element>(i));
+}
+
+void updateDocument(AdvancedDocumentView view)
+{
+    for (auto& e : view)
+        e->foo();
+}
+
+void printDocument(AdvancedDocumentConstView view)
+{
+    std::cout << "<AdvancedDocument>\n";
+    for (auto const& e : view)
+        e->print(1);
+    std::cout << "</AdvancedDocument>" << std::endl;
+}
+
+int main(int, char**) {
+    Document xs;
+    loadDocument(xs);
+    printDocument(viewOf(xs));
+    updateDocument(viewOf(xs));
+    printDocument(constViewOf(xs));
+
+    AdvancedDocument ys;
+    loadDocument(ys);
+    printDocument(viewOf(ys));
+    updateDocument(viewOf(ys));
+    printDocument(constViewOf(ys));
+
+    using V = ViewTypeFromCTVar(xs);
+    using CV = ConstViewTypeFromCTVar(xs);
+
+    auto const& info_v = typeid(V);
+    auto const& info_cv = typeid(CV);
+    std::cout << "Name  V: " << info_v.name() << std::endl;
+    std::cout << "Name CV: " << info_cv.name() << std::endl;
+
+
+    return 0;
+}
